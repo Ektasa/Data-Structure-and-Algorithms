acc6efa83a648ef57f0d02e94ec1c9ad55a2546a
Ekta Sahu <83973342+Ektasa@users.noreply.github.com>
2026-09-05 19:38:46 +0530

    [242] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ Daily problem/242-Valid-Anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int l1=s.length(); 
+        int l2=t.length();
+
+        if(l1!=l2) return false;
+
+        int[] arr=new int[26];
+        for(int i =0;i<l1;i++)
+        {
+            arr[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<l2;i++)
+        {
+            arr[s.charAt(i)-'a']--;
+        }
+
+        for(int j:arr)
+        {
+            if(j!=0) return false;
+        }
+      return true;
+        
+    }
+}
