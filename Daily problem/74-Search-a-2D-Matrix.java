50374d06a6894e962051ff99314a9a2c784b61d5
Ekta Sahu <83973342+Ektasa@users.noreply.github.com>
2026-09-19 22:15:33 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 79.40%] - LeetPush

---
+++ Daily problem/74-Search-a-2D-Matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m=matrix.length;
+        int n=matrix[0].length;
+
+        int left=0; int right=m*n-1;
+        while(left<=right)
+        {
+            int pos=(right+left)/2;
+            int row=pos/n;
+            int col=pos%n;
+            if(matrix[row][col]==target){
+                return true;
+            }
+            if(matrix[row][col]>target)
+            {
+                right=pos-1;
+            }
+            else
+            {
+                left=pos+1;
+            }
+        }
+        return false;
+    }
+}
