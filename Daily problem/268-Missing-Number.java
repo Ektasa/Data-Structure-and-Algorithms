fb370f3b0871867915575b40b97bb689fa9f468d
Ekta Sahu <83973342+Ektasa@users.noreply.github.com>
2026-09-24 00:18:22 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 48.68%] - LeetPush

--- Daily problem/268-Missing-Number.java
+++ Daily problem/268-Missing-Number.java
@@ -1,13 +1,26 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]==i)
+        //     continue;
+        //     else
+        //     return i;
+        // }
+        // return nums.length;
+
+        //XOR 
+        int mis=0; int t=0;
+        for(int i=0;i<nums.length+1;i++)
+        {
+            mis^=i;
+        }
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]==i)
-            continue;
-            else
-            return i;
+            mis^=nums[i];
         }
-        return nums.length;
+        return mis;
+
     }
 }
